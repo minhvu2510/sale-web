cca48c7dd0e4bcddb435605f18d176a07475d4c8
minhvu <vunguyenminh@vccorp.vn>
2020-12-25 17:31:23 +0700

    Add: Add content sidebar

--- src/header/header.vue
+++ src/header/header.vue
@@ -85,57 +85,134 @@
             <li>
               <div class="sidebar-group first">
                 <p class="sidebar-heading">
-                  <span>Dev</span>
+                  <!--<span>Dev</span>-->
                 </p>
                 <ul class="sidebar-group-items">
-                  <li><a href="/vuesax/development/" class="sidebar-link">Getting Started</a></li>
+                  <!--<li><a href="/vuesax/development/" class="sidebar-link">Getting Started</a></li>-->
                 </ul>
-                <div class="">
-                  <vs-slider step=10 v-model="value1"/>
-                  <vs-slider ticks step=25 v-model="value2"/>
+                <!--<div style="margin: 15px 15px 15px 15px" class="tip custom-block">-->
+                  <!--<div class="custom-block-title">Tip</div>-->
+                  <!--<p>Vuesax uses the <strong>Google Material Icons</strong> font library. For a list of all available icons, visit the official <a href="https://material.io/icons/" target="_blank" rel="noopener noreferrer">Material Icons page<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" x="0px" y="0px" viewBox="0 0 100 100" width="15" height="15" class="icon outbound"><path fill="currentColor" d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"></path> <polygon fill="currentColor" points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"></polygon></svg></a>.</p>-->
+                <!--</div>-->
+                <vs-alert @click="tipSidebar=!tipSidebar" :active="tipSidebar" icon="cancel" >
+                  <span>Sử dụng bộ lọc để tìm kiếm dễ dàng hơn <b>!</b></span>
+                </vs-alert>
+                <div class="content_side" v-bind:class="{'style-select': active_sidebar.cost} " @click="clickSidebar('cost')">
+                  <div style="margin-top: 10px">
+                    <strong>Khoảng giá</strong>
+                  </div>
+                  <div class="centerx">
+                    <!--<vs-input class="inputx" placeholder="Thấp nhất" v-model="value1"/>-->
+                    <vs-input label-placeholder="Thấp nhất" v-model="suggess_cost.value_low"/>
+                  </div>
+                  <div v-show="suggess_cost.low" class="centerx" style="padding-top: 10px" slot="demo">
+                    <vs-chip transparent color="primary" v-for="cost in suggess_value" :key="cost" @click="suggess_cost.value_low = cost">
+                      {{cost}}
+                    </vs-chip>
+                  </div>
+                  <div class="centerx" style="padding-top: 10px">
+                    <vs-input label-placeholder="Cao nhất" v-model="value4"/>
+                  </div>
+                  <div v-show="suggess_cost.hight" class="centerx" style="padding-top: 5px" slot="demo">
+                    <vs-chip transparent color="primary" v-for="cost in suggess_value" :key="cost">
+                      {{cost}}
+                    </vs-chip>
+                  </div>
+                  <div style="padding-top: 10px;padding-bottom: 10px" class="price-cover">
+                    <vs-button type="line" class="price-cover">Áp dụng</vs-button>
+                  </div>
                 </div>
+              </div>
+              <div style="text-align: left"  class="content_side" v-bind:class="{'style-select': active_sidebar.type}" @click="clickSidebar('type')">
+                <div style="text-align: center">
+                  <strong>Loại</strong>
+                </div>
+                <div style="padding-left: 20px">
+                  <input type="checkbox" name="vehicle1" value="Bike">
+                  <label for="vehicle1"> Refrigerators & Freezers (5)</label><br>
+
+                  <input type="checkbox"  name="vehicle1" value="Bike">
+                  <label for="vehicle1"> Televisions (4)</label><br>
+
+                  <input type="checkbox"  name="vehicle1" value="Bike">
+                  <label for="vehicle1"> Water & Air Purifiers (3)</label><br>
+
+                  <input type="checkbox"  name="vehicle1" value="Bike">
+                  <label for="vehicle1"> Washing Machines (2)</label><br>
+
+                  <input type="checkbox"  name="vehicle1" value="Bike">
+                  <label for="vehicle1"> Air Conditioners (2)</label><br>
+                </div>
+
+                <div style="padding-top: 10px;padding-bottom: 10px; text-align: center" class="price-cover">
+                  <vs-button type="line" class="price-cover">Áp dụng</vs-button>
+                </div>
+
+              </div>
+              <div style="text-align: left"  class="content_side" v-bind:class="{'style-select': active_sidebar.available}" @click="clickSidebar('available')">
+                <div style="text-align: center">
+                  <strong>Còn hàng</strong>
+                </div>
+                <div style="padding-left: 20px">
+                  <div>
+                    <label for="">Chỉ tìm khi còn hàng</label>
+                    <vs-switch color="success" v-model="switch2" vs-icon="done"/>
+                  </div>
+                </div>
+
+                <div style="padding-top: 10px;padding-bottom: 10px; text-align: center" class="price-cover">
+                  <vs-button type="line" class="price-cover">Áp dụng</vs-button>
+                </div>
+
+              </div>
+              <div>
+                <vs-alert :active="inputValid" color="danger" icon="new_releases" >
+                  <span>the value is <b>invalid</b> you can only enter numbers</span>
+                </vs-alert>
               </div>
             </li>
 
           </ul>
         </div>
       </div>
-      <div class="sidebar-map">
-        <div class="sidebar-links-map">
-          <div style="padding: 10px 30px 10px 3px;border-left: 1px;margin: 10px 5px 10px 5px">
-            <vs-slider step=10 v-model="value1"/>
-            <vs-slider ticks step=25 v-model="value2"/>
-            <div class="tip custom-block"></div>
-            <div class="tip custom-block" style="width: 100%">
-              <p class="custom-block custom-block-title">Giá</p>
-              <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+      <!--<div class="sidebar-map">-->
+        <!--<div class="sidebar-links-map">-->
+          <!--<div style="padding: 10px 30px 10px 3px;border-left: 1px;margin: 10px 5px 10px 5px">-->
+            <!--<vs-slider step=10 v-model="value1"/>-->
+            <!--<vs-slider ticks step=25 v-model="value2"/>-->
+            <!--<div class="tip custom-block"></div>-->
+            <!--<div class="tip custom-block" style="width: 400px">-->
+              <!--<p class="custom-block custom-block-title">Giá</p>-->
+              <!--<p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>-->
               <!--<vs-slider step=10 v-model="value1"/>-->
 
-            </div>
-          </div>
-        </div>
-        <!--<div class="c-sidebar-map">-->
-        <!--<ul class="sidebar-links-map">-->
-        <!--<vs-avatar color="success" icon="shopping_cart" badge-color="rgb(140, 23, 164)" badge="10" />-->
-        <!--</ul>-->
-        <!--</div>-->
-        <div class="con-carbon-noti">
-          <!--<div class="notiads">-->
-            <!--<div class="sponsor-special">-->
-              <!--<div class="sponsor-s">-->
-                <!--<a target="_blank" href="https://www.patreon.com/bePatron?c=1567892">-->
-                  <!--<img src="https://lusaxweb.github.io/vuesax/patreon/06-Vuesax-Diamond-Manuel-Rovira-Luis-Daniel-Rovira-Lusax-Web-Framework-ui-components-Vue-js-nuxt-vue.png" alt="">-->
-                <!--</a>-->
-                <!--<br/>-->
-                <!--<a target="_blank" href="https://www.patreon.com/bePatron?c=1567892">-->
-                  <!--<img src="https://lusaxweb.github.io//vuesax/patreon/01-Vuesax-Gold-Manuel-Rovira-Luis-Daniel-Rovira-Lusax-Web-Framework-ui-components-Vue-js-nuxt-vuepre.png" alt="">-->
-                <!--</a>-->
-                <!--<br/>-->
-              <!--</div>-->
             <!--</div>-->
+            <!--<h1 style="text-align: center">minhvu</h1>-->
           <!--</div>-->
-        </div>
-      </div>
+          <!--<h1 style="text-align: center">minhvu</h1>-->
+        <!--</div>-->
+        <!--&lt;!&ndash;<div class="c-sidebar-map">&ndash;&gt;-->
+        <!--&lt;!&ndash;<ul class="sidebar-links-map">&ndash;&gt;-->
+        <!--&lt;!&ndash;<vs-avatar color="success" icon="shopping_cart" badge-color="rgb(140, 23, 164)" badge="10" />&ndash;&gt;-->
+        <!--&lt;!&ndash;</ul>&ndash;&gt;-->
+        <!--&lt;!&ndash;</div>&ndash;&gt;-->
+        <!--<div class="con-carbon-noti">-->
+          <!--&lt;!&ndash;<div class="notiads">&ndash;&gt;-->
+            <!--&lt;!&ndash;<div class="sponsor-special">&ndash;&gt;-->
+              <!--&lt;!&ndash;<div class="sponsor-s">&ndash;&gt;-->
+                <!--&lt;!&ndash;<a target="_blank" href="https://www.patreon.com/bePatron?c=1567892">&ndash;&gt;-->
+                  <!--&lt;!&ndash;<img src="https://lusaxweb.github.io/vuesax/patreon/06-Vuesax-Diamond-Manuel-Rovira-Luis-Daniel-Rovira-Lusax-Web-Framework-ui-components-Vue-js-nuxt-vue.png" alt="">&ndash;&gt;-->
+                <!--&lt;!&ndash;</a>&ndash;&gt;-->
+                <!--&lt;!&ndash;<br/>&ndash;&gt;-->
+                <!--&lt;!&ndash;<a target="_blank" href="https://www.patreon.com/bePatron?c=1567892">&ndash;&gt;-->
+                  <!--&lt;!&ndash;<img src="https://lusaxweb.github.io//vuesax/patreon/01-Vuesax-Gold-Manuel-Rovira-Luis-Daniel-Rovira-Lusax-Web-Framework-ui-components-Vue-js-nuxt-vuepre.png" alt="">&ndash;&gt;-->
+                <!--&lt;!&ndash;</a>&ndash;&gt;-->
+                <!--&lt;!&ndash;<br/>&ndash;&gt;-->
+              <!--&lt;!&ndash;</div>&ndash;&gt;-->
+            <!--&lt;!&ndash;</div>&ndash;&gt;-->
+          <!--&lt;!&ndash;</div>&ndash;&gt;-->
+        <!--</div>-->
+      <!--</div>-->
       <br/>
       <!--<div>-->
       <!--<vs-button @click="myFilter" color="primary" type="border">Primary</vs-button>-->
@@ -172,7 +249,11 @@ export default {
   data () {
     return {
       isActive: false,
-      noSideba: false
+      noSideba: false,
+      active_sidebar: {'cost': false, 'type': false, 'available': false},
+      tipSidebar: true,
+      suggess_cost: {'low': false, 'hight': false,'value_low': ''},
+      suggess_value: ['100k', '200k', '500k', '1000k', '1500k']
     }
   },
 
@@ -183,6 +264,30 @@ export default {
       this.noSideba = !this.noSideba
       console.log(this.isActive)
       // some code to filter users
+    },
+    clickSidebar (keyCheck) {
+      if (keyCheck === 'cost') {
+        this.active_sidebar.cost = true
+        this.active_sidebar.type = false
+        this.active_sidebar.available = false
+        this.suggess_cost.low = true
+        this.suggess_cost.hight = true
+      }
+      if (keyCheck === 'type') {
+        this.active_sidebar.type = true
+        this.active_sidebar.cost = false
+        this.active_sidebar.available = false
+        this.suggess_cost.low = false
+        this.suggess_cost.hight = false
+      }
+      if (keyCheck === 'available') {
+        this.active_sidebar.available = true
+        this.active_sidebar.type = false
+        this.active_sidebar.cost = false
+        this.suggess_cost.low = false
+        this.suggess_cost.hight = false
+      }
+      console.log('anh chi la ke ngoc nhung nho dieu xa voi')
     }
   }
 }
@@ -208,5 +313,40 @@ export default {
   align-items: center;
   -ms-flex-pack: center;
   justify-content: center;
+}
+  .content_side {
+    height: 60%;
+    margin: 10px 10px 10px 10px;
+    background-color: white;
+  }
+
+.style-select {
+  background: rgba(0, 196, 244, 0.05);
+  border: 1px solid #0088cc;
+  /*color: #676767;*/
+  color: rgba(31,116,255,1);
+  .el-card__body {
+  .price-cover {
+  color: #0088cc;
+  }
+  }
+}
+
+.inline {
+  display: inline-block;
+  margin: 1em;
+}
+
+.wrap {
+  display: table;
+  padding-left: 20px;
+  margin-left: 20px;
+}
+
+.wrap vs-checkbox {
+  padding-left: 20px;
+  margin-left: 20px;
+  display:        table-cell;
+  vertical-align: middle;
 }
 </style>
